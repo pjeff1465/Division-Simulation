7be0c09762254773df482b136b13b96c68b8f641
admin <admin@PP-Pro.local>
2025-04-06 17:38:40 -0500

    Fixed seperate funcs

--- funcs.py
+++ funcs.py
@@ -1,14 +1,26 @@
-# Find bit position of a based on index 
-def BitPosition(a, index):
-    return(a & (1 << index)) != 0
-# Returns true if index of a is 1, and false if 0
+# Find bit position of q based on index 
+# q = integer
+def BitPosition(q, length):
+    bin_str = format(q, f'0{length}b') # format binary string WITH leading zeros
 
-# Perform a = a + (-b)
-def SubBinary(a, b):
-    while b != 0:
-        borrow = (~a) & b
-        a = a ^ b
-        b = borrow << 1
-    return a
-# Returns integer format of a - b 
+    return bin_str[0] == '1' # check first element of string
+# Returns true if index of q is 1, and false if 0
 
+# Perform q = q + (-m)
+# q, m = integer
+def SubBinary(q, m):
+    while m != 0:
+        carry = (~q) & m
+        q = q ^ m
+        m = carry << 1
+    return q
+# Returns integer format of q - m
+
+# Perform q = q + m ; Where q, m = integer
+def AddBinary(q, m):
+    while m != 0:
+        carry = q & m
+        q = q ^ m
+        m = carry << 1
+    return q 
+# Returns q = q + m
--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from funcs import *
 
 if __name__ == "__main__":
+    # q = dividend
+    # m = divisor 
+
     # Check SubBinary function
     # two binary numbers as strings
-    a_bin = input("Enter the first number (binary): ")
-    b_bin = input("Enter the second number (binary): ")
+    q_bin = input("Enter the first number (binary): ")
+    #b_bin = input("Enter the second number (binary): ")
 
     # convert binary strings to integers
-    a = int(a_bin, 2)
-    b = int(b_bin, 2)
+    q = int(q_bin, 2)
+    #m = int(m_bin, 2)
 
-    print(f"numbers in binary: {a_bin}, {b_bin}")
+    print(f"numbers in binary: {q_bin}")
 
-    result = SubBinary(a, b)
+    #result = SubBinary(q, m)
+    #result = AddBinary(q, m)
 
-    print(f"Result (integer): {result}")
-    print(f"Result (binary): {format(result, 'b')}") # binary format with 2's comp handling
+    #print(f"Result (integer): {result}")
+    #print(f"Result (binary): {format(result, 'b')}") # binary format with 2's comp handling
 
     # Check bit position function
-    index = int(input(f"Enter index for bit position of {a_bin}: "))
-    position = BitPosition(a, index)
+    # Check MSB of a (integer)
+    length = len(q_bin)
+    position = BitPosition(q, length)
 
-    print(f"Bit of {a_bin} with index {index} is: {'1' if result else '0'}")
+    print(f"leftmost bit of {q_bin} is: {position}")
+    print(length)
