896e7a25c994f61aa6093234ecead7ae3121b94c
bdccxf <bdccxf@umsystem.edu>
2025-04-07 16:11:45 -0500

    Update nonrestore.py

--- nonrestore.py
+++ nonrestore.py
@@ -2,11 +2,6 @@
 Check Overflow
   if Q >= M then there is overflow (funcs.py for overflow)
     return error
-Check XOR of Q and M (funcs.py for XOR?!)
-  if 1
-    return true
-  if 0
-    return false
 do while n >= 0 (n = number of bits in Q)
   Check Sign Bit of A (funcs.py for BitPosition(a) true = neg)
     if = 0, SHL, A=A-M (funcs.py for sub)
@@ -16,9 +11,8 @@ do while n >= 0 (n = number of bits in Q)
     if =1, Q[1] = 0
   n--
   i++ (iteration counter int i = 0)
-
-if result != XOR of Q and M
- reult = ~result (2s complement)
+if sign bit of a = 1
+  a = a+ m
 """
 from funcs import *
 
@@ -31,13 +25,11 @@ def NonRestoring(q, m, length):
     if((CheckOverflow(q, m, length)) == True): # overflow has occured!
         print(f'Overflow has occurred! Exiting program.')
         exit
-    else:
-      #Check XOR of Q and M
-
+      
       #do while loop
       while n >= 0:
           # Perofmr SHL
-          ShiftLeft(a, q, length)
+          a, q = ShiftLeft(a, q, length)
           n -= 1 # -1 for SHL
 
           if BitPosition(a) == True: # A is negative
@@ -56,9 +48,7 @@ def NonRestoring(q, m, length):
               q[0] = 1 
           
           i = i + 1 
-
-      #Check Reult with XOR of Q and M  
-
+        
     # On last iteration if A is negative => A <- A + M
     if BitPosition(a) == True: 
         a = AddBinary(a, m)
